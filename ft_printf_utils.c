3c0b14ccf6308f2d7107d4920ea8882694fadb22
romeo rodor <romeo.rodor@gmail.com>
2023-02-28 12:13:04 +0100

    add putnbrbase

--- ft_printf_utils.c
+++ ft_printf_utils.c
@@ -14,6 +14,8 @@
 
 void	ft_putchar(char c)
 {
+	if (c < 10 && c >= 0)
+		c += 48;
 	write (1, &c, 1);
 }
 
@@ -39,9 +41,49 @@ int		ft_strlen(char *str)
 	return (i);
 }
 
-int		ft_putnbr_base(int n, int l, int b)
+int		ft_putnbr_base(int n, int l, char *base)
 {
+	int	lbase;
 	char	str[32];
+	int	i;
 
-
+	i = 0;
+	lbase = ft_strlen(base);
+	//printf("test");
+	if (n == 0)
+	{
+		ft_putchar(base[i]);
+		l++;
+		return (l);
+	}
+	while (n > 0)
+	{
+		//printf("test");
+		str[i] = base[n % lbase];
+		i++;
+		n = n / lbase;
+		l++;
+	}
+	i--;
+	while (i >= 0)
+	{
+		ft_putchar(str[i]);
+		i--;
+	}
+	return (l);
 }
+
+/*
+#include <unistd.h>
+#include <stdio.h>
+int main()
+{
+	char	base[] = "0123456789ABCDEF";
+
+	ft_putnbr_base(76, 0, base);
+	write(1, "\n", 1);
+	return (0);
+}*/
+
+
+
